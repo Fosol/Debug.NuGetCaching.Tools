45e61518af9ec50fd19e23ad99d6dd5ed9b10eaa
fosol <jeremymfoster@hotmail.com>
2016-02-05 13:41:10 -0800

    test

---
+++ src/NuGetCaching.Tool1/Employee.cs
@@ -0,0 +1,22 @@
+﻿using NuGetCaching.Tool2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NuGetCaching.Tool1
+{
+    public class Employee : Person
+    {
+        public int Number { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {this.Number}";
+        }
+    }
+}
---
+++ src/NuGetCaching.Tool2/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NuGetCaching.Tool2
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
